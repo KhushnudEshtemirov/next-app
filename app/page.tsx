f409c98de49b365252d06199ce1680df7667fe5c
Khushnud Eshtemirov <khushnudeshtemirov@gmail.com>
2024-07-30 11:35:33 +0500

    Added react hook form and zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,49 @@
+"use client";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import "./styles.scss";
+
+const schema = z.object({
+  email: z.string().email("Enter valid email address"),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" })
+    .max(15, { message: "Password must be less than 15 characters" }),
+});
+
+type FormFields = z.infer<typeof schema>;
+
 export default function Page() {
-  return <h1>Hello World</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleFormSubmit: SubmitHandler<FormFields> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
+  return (
+    <div className="p-5">
+      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit(handleFormSubmit)} className="tutorial">
+        <input {...register("email")} type="text" />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <input {...register("password")} type="password" />
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+        <button disabled={isSubmitting} type="submit">
+          {isSubmitting ? "Loading..." : "Submit"}
+        </button>
+      </form>
+    </div>
+  );
 }
